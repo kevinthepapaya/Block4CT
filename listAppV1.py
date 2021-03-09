4f947c974acd8f35f49bc9822a527fa5aee73efa
kevinthepapaya <79876520+kevinthepapaya@users.noreply.github.com>
2021-03-09 16:32:31 -0700

    Add files via upload

---
+++ listAppV1.py
@@ -0,0 +1,25 @@
+"""
+
+Program goals:
+1. Get user input
+2. Convert the input to input
+3. Add input to list
+4. Fill values from specific input positions
+"""
+#create functions that will perform those actions above
+def mainProgram():
+    myList = []
+    print ("Hello!")
+    print("Choose one of the following options. Type numbers ONLY!")
+    choice = input("!. Add to list, 2. Return the value at an index position  ")
+    if choice == "1":
+        addToList()
+    elif choice == "2":
+        indexValues
+        #We need to add an exit program and error catching
+def addToList():
+def indexValues():
+
+
+if __name__ == "__main__":
+    mainProgram()
